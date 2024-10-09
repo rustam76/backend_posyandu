74ec0fe1a0fef371123b3569083ff1fdf86b29c3
majid <rustammajid76@gmail.com>
2024-10-09 16:08:37 +0700

    perubahan configurasi

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const app = express();
 const routes = require('./src/routes/routes');
-const authRoutes = require('./src/routes/authRoute'); // Auth routes
 
 
 app.use(express.json());
+
+// API routes with JWT protection
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// Auth routes
-app.use('/auth', authRoutes);
-
-// API routes with JWT protection
-app.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_URI, {
-    dialect: 'mysql',
-});
+// Use individual environment variables for DB connection
+const sequelize = new Sequelize(
+    process.env.DB_NAME, // Database name
+    process.env.DB_USER, // Database username
+    process.env.DB_PASSWORD, // Database password
+    {
+        host: process.env.DB_HOST, // Database host
+        dialect: 'mysql', // Database dialect (MySQL in this case)
+    }
+);
 
 const connectDB = async () => {
     try {
